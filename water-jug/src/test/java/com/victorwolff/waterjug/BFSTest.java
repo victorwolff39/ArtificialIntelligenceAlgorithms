b228ebf21b8342b8e8a40ee1333d85c28ad3968c
Victor Wolff <victorwolff39@gmail.com>
2022-04-28 02:00:36 -0300

    Fixing water jug test

--- water-jug/src/test/java/com/victorwolff/waterjug/BFSTest.java
+++ water-jug/src/test/java/com/victorwolff/waterjug/BFSTest.java
@@ -10,18 +10,16 @@ class BFSTest {
     void shouldSolveBFSCorrectly() {
         BFS bfs = new BFS(4, 3, 2);
         String expected = """
-                (0, 0)\s
-                (0, 3)\s
-                (4, 0)\s
-                (4, 3)\s
-                (3, 0)\s
-                (1, 3)\s
-                (3, 3)\s
-                (4, 2)\s
-                (0, 2)""";
+                0 , 0
+                0 , 3
+                3 , 0
+                3 , 3
+                4 , 2
+                0 , 2
+                """;
 
         String answer = bfs.solve();
 
-        assertEquals(answer.trim(), expected.trim());
+        assertEquals(expected, answer);
     }
 }
